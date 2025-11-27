3de784517818d1e545797563349f2f7c19d13629
jelius-sama <jelius.basumatary.sama@gmail.com>
2025-11-27 20:01:43 +0530

    synced local changes

--- libgolang/libgolang.h
+++ libgolang/libgolang.h
@@ -12,8 +12,6 @@
 
 #ifndef GO_CGO_GOSTRING_TYPEDEF
 typedef struct { const char *p; ptrdiff_t n; } _GoString_;
-extern size_t _GoStringLen(_GoString_ s);
-extern const char *_GoStringPtr(_GoString_ s);
 #endif
 
 #endif
@@ -72,15 +70,9 @@ typedef size_t GoUintptr;
 typedef float GoFloat32;
 typedef double GoFloat64;
 #ifdef _MSC_VER
-#if !defined(__cplusplus) || _MSVC_LANG <= 201402L
 #include <complex.h>
 typedef _Fcomplex GoComplex64;
 typedef _Dcomplex GoComplex128;
-#else
-#include <complex>
-typedef std::complex<float> GoComplex64;
-typedef std::complex<double> GoComplex128;
-#endif
 #else
 typedef float _Complex GoComplex64;
 typedef double _Complex GoComplex128;
@@ -138,7 +130,7 @@ extern void* ChannelRecv(uint64_t handle);
 extern int ChannelTryRecv(uint64_t handle, void** valuePtr);
 extern void ChannelClose(uint64_t handle);
 extern char* HttpRegisterRoute(char* path, char* response);
-extern uint64_t HttpGetRequestCount(void);
+extern uint64_t HttpGetRequestCount();
 extern char* HttpStartServer(char* addr);
 
 #ifdef __cplusplus
